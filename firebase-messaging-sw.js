a2c529064f9f006d97e6ca406fe0c619c2d7db0d
Colman Mwakio <cmwakio@gmail.com>
2018-10-26 12:56:10 +0300

    check user domain

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,24 +4,24 @@ importScripts('https://www.gstatic.com/firebasejs/4.6.2/firebase-messaging.js');
 
 // Initialize Firebase
 // Public App
-// var config = {
-//     apiKey: "AIzaSyAf_oJi6fgm6-ZrVPeOVdW2DDoeu3v5SxY",
-//     authDomain: "leadershipplatform-158316.firebaseapp.com",
-//     databaseURL: "https://leadershipplatform-158316.firebaseio.com",
-//     projectId: "leadershipplatform-158316",
-//     storageBucket: "leadershipplatform-158316.appspot.com",
-//     messagingSenderId: "438289288307"
-// };
+var config = {
+    apiKey: "AIzaSyAf_oJi6fgm6-ZrVPeOVdW2DDoeu3v5SxY",
+    authDomain: "leadershipplatform-158316.firebaseapp.com",
+    databaseURL: "https://leadershipplatform-158316.firebaseio.com",
+    projectId: "leadershipplatform-158316",
+    storageBucket: "leadershipplatform-158316.appspot.com",
+    messagingSenderId: "438289288307"
+};
 
 // Test App
-var config = {
-    apiKey: "AIzaSyBJH4pbQEzEpxtEomcnYjlUfsiFC5nqHoQ",
-    authDomain: "glp-test.firebaseapp.com",
-    databaseURL: "https://glp-test.firebaseio.com",
-    projectId: "glp-test",
-    storageBucket: "glp-test.appspot.com",
-    messagingSenderId: "805735980290"
-};
+// var config = {
+//     apiKey: "AIzaSyBJH4pbQEzEpxtEomcnYjlUfsiFC5nqHoQ",
+//     authDomain: "glp-test.firebaseapp.com",
+//     databaseURL: "https://glp-test.firebaseio.com",
+//     projectId: "glp-test",
+//     storageBucket: "glp-test.appspot.com",
+//     messagingSenderId: "805735980290"
+// };
 
 firebase.initializeApp(config);
 
